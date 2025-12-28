96ce97bca575f6ac64cf23bfaf6397f8e30ccc20
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2025-12-28 21:35:19 +0530

    Added model layer and started presentation layer

---
+++ src/main/java/com/darklord/url_shortener/url/UrlShortener.java
@@ -0,0 +1,77 @@
+package com.darklord.url_shortener.url;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+
+@Entity
+@Table
+public class UrlShortener {
+
+    @Id
+    @SequenceGenerator (
+        name = "url_sequence",
+        sequenceName = "url_sequence",
+        allocationSize = 1
+    )
+    @GeneratedValue (
+        strategy = GenerationType.SEQUENCE,
+        generator = "url_sequence"
+    )
+    private Long id;
+    private String shortCode;
+    private String originalUrl;
+    private LocalDate createdAt;
+
+    public UrlShortener(String shortCode,String originalUrl) {
+        this.shortCode = shortCode;
+        this.originalUrl = originalUrl;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getShortCode() {
+        return shortCode;
+    }
+
+    public void setShortCode(String shortCode) {
+        this.shortCode = shortCode;
+    }
+
+    public String getOriginalUrl() {
+        return originalUrl;
+    }
+
+    public void setOriginalUrl(String originalUrl) {
+        this.originalUrl = originalUrl;
+    }
+
+    public LocalDate getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDate createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return  "UrlShortener {" +
+                "id: "+id +
+                "shortCode: "+shortCode +
+                "originalUrl: "+originalUrl+
+                "created at: "+createdAt + 
+                "}";
+    }
+}
