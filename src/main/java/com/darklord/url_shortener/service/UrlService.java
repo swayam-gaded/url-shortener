c10f1a2ffc7259184b1533842a2270650b6e2f4d
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2026-03-06 09:38:06 +0530

    Cleaned out the previous shortCodeGenerator

--- src/main/java/com/darklord/url_shortener/service/UrlService.java
+++ src/main/java/com/darklord/url_shortener/service/UrlService.java
@@ -26,34 +26,6 @@ public class UrlService {
         UrlShortener url = urlOptional.get();
         return url.getOriginalUrl();
     }
-    public String shortCodeGenerator() {
-        StringBuilder shortUrl = new StringBuilder();
-        for(int i=0;i<5;i++) {
-            int random = (int)(Math.random()*3);
-            switch (random) {
-                case 0:
-                    char randomNum = (char)(Math.random()*10+48);
-                    shortUrl.append(randomNum);
-                    break;
-                case 1:
-                    char randomCapAlpha = (char)(Math.random()*26+65);
-                    shortUrl.append(randomCapAlpha);
-                    break;
-                case 2:
-                    char randomSmallAlpha = (char)(Math.random()*26+97);
-                    shortUrl.append(randomSmallAlpha);
-                    break;
-                default:
-                    break;
-            }
-        }
-        return shortUrl.toString();
-    }
-
-    public boolean existShortCode(String shortCode) {
-        Optional<UrlShortener> existingUrl = urlRepo.findByShortCode(shortCode);
-        return existingUrl.isPresent();
-    }
 
     public void addNew(UrlShortener urlShortener) {
         Optional<UrlShortener> urlOptional = urlRepo.findByOriginalUrl(urlShortener.getOriginalUrl());
