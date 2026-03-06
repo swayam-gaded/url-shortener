adcffacc72b7c1f53617d16f98dc5aee23a6307d
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2026-03-06 09:19:33 +0530

    Base62 Encoding Added

---
+++ src/main/java/com/darklord/url_shortener/util/base62util.java
@@ -0,0 +1,20 @@
+package com.darklord.url_shortener.util;
+
+public class base62util {
+    private static final String ALLOWED_CHARACTERS = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private static final int BASE = ALLOWED_CHARACTERS.length();
+
+    public static String encode(long input) {
+        StringBuilder encodedString = new StringBuilder();
+
+        if(input == 0) {
+            return String.valueOf(ALLOWED_CHARACTERS.charAt(0));
+        }
+
+        while(input > 0) {
+            encodedString.append(ALLOWED_CHARACTERS.charAt((int)(input%BASE)));
+            input = input/BASE;
+        }
+        return encodedString.reverse().toString();
+    }
+}
