0f4ebb8fc637bafaff450e69a818dcd6f36e46f9
swayam-gaded <230393153+swayam-gaded@users.noreply.github.com>
2026-03-16 23:33:22 +0530

    Finished testing for Controller

--- dev_notes/Phase_4.md
+++ dev_notes/Phase_4.md
@@ -11,12 +11,14 @@ Difference between Unit tests and Integration tests <br>
 | Dependency| @Mock | @MockitoBean|
 | Performance | Extremely Fast cuz no Spring Boot Required | Slower cuz it starts the spring context partially | 
 
-UrlControllerTest<br>
+### UrlControllerTest<br>
 The `@MockMvcTest` annotation is used to test only the API endpoints by starting a Web layer, making it faster and more focused. The alternative or other side is `@SpringBootTest`<br>
 `MockMvc` acts as the fake browser which sends the http requests into the code without using the port 8080 allowing us to check whether the controller is correctly mapped to the path specified<br>
-The line `Mockito.when(...).thenReturn(...)` is called **stubbing** and is used to return a fake response to verify whether the controller is accepting the request properly and is giving the expected response<br>
+The line `Mockito.when(<br>.).thenReturn(<br>.)` is called **stubbing** and is used to return a fake response to verify whether the controller is accepting the request properly and is giving the expected response<br>
 `ObjectMapper` is used to java objects into JSON strings and here we are using the `@RequestBody` annotation to convert the json request to the UrlRequest DTO<br>
-The test failed in the beginning cuz of `@EnableJpaAuditing`. The location of this annotation must not be on the main application class cuz this breaks the slice tests(integration) and this led to creating the `JpaConfig` file<br> 
+The test failed in the beginning cuz of `@EnableJpaAuditing`. The location of this annotation must not be on the main application class cuz this breaks the slice tests(integration) and this led to creating the `JpaConfig` file<br>
+JSONPath: Learned to navigate both single objects ($.field) and arrays ($[0].field or $.length())<br>
+Don't ignore the stack trace: We learned that a 500 error in a test usually points to a NullPointerException because a Mock was not stubbed correctly<br>
 
 ### Extra Points 
 - `@ReqArgsConstructor` - now the required arguments are the ones defined as `final` or annotated with `@NonNull`
--- src/test/java/com/darklord/url_shortener/controller/UrlControllerTest.java
+++ src/test/java/com/darklord/url_shortener/controller/UrlControllerTest.java
@@ -4,6 +4,7 @@ import com.darklord.url_shortener.dto.UrlRequest;
 import com.darklord.url_shortener.dto.UrlResponse;
 import com.darklord.url_shortener.service.UrlService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.persistence.EntityNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,10 +13,15 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(UrlController.class)
 public class UrlControllerTest {
@@ -26,6 +32,7 @@ public class UrlControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    // A valid request hitting the POST endpoint
     @Test
     void whenPostValidUrl() throws Exception {
         UrlRequest request = new UrlRequest("https://google.com");
@@ -37,5 +44,62 @@ public class UrlControllerTest {
                         .content(objectMapper.writeValueAsString(request)))
                         .andExpect(status().isCreated())
                         .andExpect(jsonPath("$.shortCode").value("DEMO"));
+    }
+
+    // Invalid request hitting the POST endpoint
+    @Test
+    void whenPostInvalidUrl() throws Exception {
+        UrlRequest request = new UrlRequest("htp//google.com");
+
+        mockMvc.perform(post("/api/v1/url")
+                    .contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(request)))
+                    .andExpect(status().isBadRequest());
+
+    }
+
+    // Valid request hitting the GET endpoint for redirection
+    @Test
+    void whenGetValidShortCode() throws Exception {
+        String ogUrl = "https://github.com";
+        String shortCode = "github";
+        Mockito.when(urlService.getOriginalUrlAndIncrementCount(shortCode)).thenReturn(ogUrl);
+
+        mockMvc.perform(get("/api/v1/url/{shortCode}",shortCode))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location",ogUrl));
+    }
+
+    //Invalid request hitting the GET endpoint for redirection
+    @Test
+    void whenGetInvalidShortCode() throws Exception {
+        Mockito.when(urlService.getOriginalUrlAndIncrementCount(anyString())).thenThrow(new EntityNotFoundException("Shortcode not found"));
+
+        mockMvc.perform(get("/api/v1/url/{shortCode}","fakeCode"))
+                .andExpect(status().isNotFound());
+    }
+
+    //Valid request hitting the GET endpoint for displaying the output
+    @Test
+    void whenGetValidRequest() throws Exception {
+        List<UrlResponse> mockResponse = new ArrayList<>();
+        mockResponse.add(new UrlResponse("hi","DEMO"));
+        Mockito.when(urlService.displayOutput()).thenReturn(mockResponse);
+
+        mockMvc.perform(get("/api/v1/url"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(1))
+                .andExpect(jsonPath("$[0].shortCode").value("hi"))
+                .andExpect(jsonPath("$[0].originalUrl").value("DEMO"));
+    }
+
+    //When there is no output to be given for the GET endpoint for display
+    @Test
+    void whenGetValidRequestWithNoOutput() throws Exception {
+        Mockito.when(urlService.displayOutput()).thenReturn(Collections.emptyList());
+
+        mockMvc.perform(get("/api/v1/url"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
     }
 }
